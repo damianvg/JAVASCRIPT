44a74ccdbced61fcf6528e2acc0dcaa34119fde1
damianvg <47354845+damianvg@users.noreply.github.com>
2019-07-01 09:44:34 +0200

    Add files via upload

---
+++ animatie.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".goed").click(function(){
+    var div = $(".vliegtuig");
+    div.animate({left: '1000px'}, "slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".goed").click(function(){
+    var div = $(".wolk");
+    div.animate({right: '1000px'}, "slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".goed").click(function(){
+    var div = $(".wolk2");
+    div.animate({right: '1000px'}, "slow");
+  });
+});
